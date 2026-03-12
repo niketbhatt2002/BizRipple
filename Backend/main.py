7c43c2682d05b9e0fbcd7f0376534302367c638f
Niket Bhatt <120054912+niketbhatt2002@users.noreply.github.com>
2026-03-12 00:49:26 -0400

    Remove unnecessary blank line in main.py

--- Backend/main.py
+++ Backend/main.py
@@ -6,7 +6,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # You can restrict this to your frontend domain later
